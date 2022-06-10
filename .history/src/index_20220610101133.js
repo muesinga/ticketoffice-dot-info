9ced443348cbbb484dc48630b768308088562680
muesinga <contact@aaronmuesing.com>
2022-06-10 10:37:52 -0700

    update

---
+++ .history/src/index_20220610101133.js
@@ -0,0 +1,13 @@
+var navlinkContainer = document.getElementById("navlink-container")
+var navlinks = navlinkContainer.getElementsByClassName("navlink")
+
+
+for (var i = 0; i < navlinks.length; i++) {
+    navlinks[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className,replace("active", "");
+        this.className += "active";
+    });
+}
+
+//
---
+++ .history/src/nav_20220610095012.js
@@ -0,0 +1,11 @@
+fetch('/components/nav.html')
+.then(res => res.text())
+.then(text => {
+    let oldelem = document.querySelector("script#replace_with_navbar");
+    let newelem = document.createElement("div");
+    newelem.innerHTML = text;
+    oldelem.parentNode.replaceChild(newelem,oldelem);
+})
+
+fetch('/guides/renewal.html')
+.then(res => res.tex)
